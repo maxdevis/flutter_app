ea2ff4a564c0f4d0050349c941c80cbb105ccc71
Max <maxdiable.test@gmail.com>
2023-05-24 15:24:50 +0200

    save drawer

--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import '../Component/myDrawer.dart';
+import '../Component/myAppBar.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,9 +12,20 @@ class HomePage extends StatefulWidget {
 class SplashScreenState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: Colors.yellow,
-        child:FlutterLogo(size:MediaQuery.of(context).size.height)
+    return Scaffold(
+        backgroundColor: Colors.green[100],
+        appBar: const MyAppBar( title: 'MyAppBar',),
+        //set app bar from appbar.dart
+        // use like this where ever you want
+        drawer: myDrawer(),
+        //set drawer from app_drawer.dart
+        //set like this where ever you want
+        body: const Center(
+            child: Text("Reusable Drawer and Appbar",
+              style: TextStyle(fontSize: 20),
+            )
+        )
     );
   }
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
-import 'Component/MyDrawer.dart';
+import 'Component/myDrawer.dart';
 import 'Pages/Splash.dart';
 
 void main() => runApp(
@@ -18,12 +18,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       locale: DevicePreview.locale(context),
-      builder: (context, child) {
-        return Scaffold(
-          drawer: const MyDrawer(),
-          body: child,
-        );
-      },
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
       home: const SplashScreen(),
